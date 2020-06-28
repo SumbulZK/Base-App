ceeebe0e1f4f0b40d8b88f2f75decca11b8fd6ab
Sumbul <sumbulzehra.hk@gmail.com>
2020-06-28 05:23:40 +0500

    Basic Edits to React App
    Basic component and custom tags added to the react app

---
+++ src/Hello.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import "./Hello.css"
+
+
+function Hello({firstname, lastname}) {
+  
+  return <div>
+    Hello from Hello.js
+<div> 
+    <p className="myname">I am <strong >{firstname} {lastname}</strong></p>
+    <h3>Importance of Time Management</h3>
+    <ul>
+        <li>Time is an asset that you can never get back </li>
+        <li>Other blessings like wealth, even if spent unwisely, can be recovered through hard work</li>
+        <li>It is important to spend time wisely, much more wisely than you spend your money</li>
+    </ul>
+</div>
+   
+    
+    </div>
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div><App firstname="Sumbul" lastname="Zehra"/> </div>,
   document.getElementById('root')
 );
 
